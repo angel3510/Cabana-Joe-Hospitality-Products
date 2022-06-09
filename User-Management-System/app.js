d91318b8f783124f2c5a801e6918e16e968cef8b
Angel Sandoval <sandovalangel10@hotmail.com>
2022-06-09 13:31:20 -0700

    Updated files

--- User-Management-System/app.js
+++ User-Management-System/app.js
@@ -1,43 +1,39 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
-const bodyParser = require('body-parser');
-const mysql = require('mysql2');
+//const bodyParser = require('body-parser'); // No longer Required
+//const mysql = require('mysql'); // Not required -> moved to userController
 
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-//parsing middleware
+// Parsing middleware
+// Parse application/x-www-form-urlencoded
+// app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true })); // New
 
-app.use(bodyParser.urlencoded({ extended: false }));
+// Parse application/json
+// app.use(bodyParser.json());
+app.use(express.json()); // New
 
-//parse application/json
-app.use(bodyParser.json());
-
-//static files
+// Static Files
 app.use(express.static('public'));
 
-//templating engine uses view and  layout folder as a default
-app.engine('hbs', exphbs.engine({ extname: '.hbs' }));
-app.set('view engine', 'hbs');
+// Templating Engine
+const handlebars = exphbs.create({ extname: '.hbs', });
+app.engine('.hbs', handlebars.engine);
+app.set('view engine', '.hbs');
 
-//connection port
-const pool = mysql.createPool({
-  connectionLimit: 100,
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-});
+// You don't need the connection here as we have it in userController
+// let connection = mysql.createConnection({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASS,
+//   database: process.env.DB_NAME
+// });
 
-//connect to db
-pool.getConnection((err, connection) => {
-  if (err) throw err; //not connected
-  console.log('connected as id ' + connection.threadId);
-});
+const routes = require('./server/routes/user');
+app.use('/', routes);
 
-const route = require('./server/routes/user');
-app.use('/', route);
-
-app.listen(port, () => console.log(`listening on port ${port}`));
+app.listen(port, () => console.log(`Listening on port ${port}`));
